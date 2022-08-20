168e9483064b6fc276e4fd314f2e0f0f2e358f0e
Mirriam <mirriamgacheri@gmail.com>
2022-08-20 17:00:48 +0300

    Add files via upload
    this version of a clock was made with vanilla Javascript, Html and Css

---
+++ index.js
@@ -0,0 +1,35 @@
+const hourEl= document.getElementById("hour")
+const minuteEl= document.getElementById("minutes")
+const secondEl= document.getElementById("seconds")
+const ampmEl= document.getElementById("ampm")
+
+function updateClock(){
+    const time =new Date().toLocaleTimeString()
+    const d= new Date().toLocaleDateString()
+    let h= new Date().getHours()
+    let m = new Date().getMonth()
+    let s= new Date().getSeconds()
+    let ampm= "AM"
+    if(h > 12){
+
+        h= h- 12       
+        ampm= "PM"
+    }
+    h= h<10 ? "0" + h : h
+    m= m<10 ? "0" + m : m
+    s= s<10 ? "0" + s : s
+
+
+     hourEl.innerHTML = h;
+     minuteEl.innerHTML = m;
+     secondEl.innerHTML = s;
+     ampmEl.innerHTML= ampm;
+     document.getElementById("date-el").innerHTML ="Today's date is : " + d + " " + time
+
+setTimeout(()=>{
+    
+    updateClock()
+
+} ,1000)
+}
+updateClock()
